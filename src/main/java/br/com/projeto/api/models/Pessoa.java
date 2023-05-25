1516daa150df375611b706d28af24f8b6a72d0bb
José Adauto <60900449+albadauto@users.noreply.github.com>
2023-05-25 14:00:22 -0300

    first commit

---
+++ src/main/java/br/com/projeto/api/models/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.projeto.api.models;
+
+public class Pessoa {
+    
+    private String nome;
+    private int Idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getIdade() {
+        return Idade;
+    }
+    public void setIdade(int idade) {
+        Idade = idade;
+    }
+    
+}
